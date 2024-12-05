aa45f27b134dd6360a948c698a4b4c5e10cc7c0a
julia <julia.lipatnikova@gmail.com>
2024-12-05 18:03:04 +0500

    dreh

--- 29.11.24/main.cpp
+++ 29.11.24/main.cpp
@@ -18,18 +18,18 @@ public:
 	int var;
 };
 
-int main() {
+void main2() {
 	int var{};
 	auto func = [&](int a, int b) -> void {
 		var = a + b;
 		};
 	func(3, 0x14);
-	
+
 	std::cout << var;
 	MyNum a(2), b(1);
 	auto max_val = std::max(a, b,
-	[](const MyNum& first, const MyNum& second) {return first.var < second.var; }
- );
+		[](const MyNum& first, const MyNum& second) {return first.var < second.var; }
+	);
 	std::vector<MyNum> arrays;
 	for (size_t i = 0; i < 26; i++)
 	{
@@ -41,7 +41,7 @@ int main() {
 		std::cout << el.var << ' ';
 
 	}
-	 
+
 	std::sort(arrays.begin(), arrays.end(),
 		[](const MyNum& first, const MyNum& second) {return first.var < second.var; }
 	);
@@ -52,6 +52,49 @@ int main() {
 		std::cout << el.var << ' ';
 
 	}
+
+
+}
+
+
+int main() {
+	int a{};
+	int b{};
+	int c{};
+
+	//void EnterNum(std::string propt, int& var) {
+	//  такую функцию нельзя создать внутри др функции
+	//}
+	
+	auto func = [](std::string propt, int& var) {
+		std::cout << propt;
+		std::string tmp;
+		std::cin >> tmp;
+		bool is_correct{ true };
+		for (auto& i : tmp)
+		{
+			is_correct = ::isdigit(i);
+			if (!is_correct)break;
+		}
+		if (is_correct) {
+            std::cin >> var;
+		}
+        
+		};
+	func("Enter first num: ", a);
+	func("Enter second num: ", b);
+    // лямбда выражения в качестве анонимной функции
+	// можно использовать как инструмент сокращения
+	// размера кодовой базы
+	// в тех случаях когда блок кода сильно засорил бы
+	// проект при выделении его в отдельной функции
+	
+	auto getResult = [&c](int a, int b) {
+		c = a + b;
+
+		};
+	getResult(a, b);
+	std::cout << "Result of action " << c;
 
 	return 0;
 }
